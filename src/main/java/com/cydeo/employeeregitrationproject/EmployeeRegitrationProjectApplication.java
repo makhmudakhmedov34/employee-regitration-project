1177d474c98cafecedcf304a857f8897a86d8937
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-03-11 23:00:25 -0500

    Employee Registration Lab

---
+++ src/main/java/com/cydeo/employeeregitrationproject/EmployeeRegitrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregitrationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegitrationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRegitrationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/employeeregitrationproject/EmployeeRegitrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregitrationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegitrationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
